9805324d0d5a53c226bfaf79297c234fe1c2772d
sebast825 <sebamolina825@gmail.com>
2025-10-23 15:40:51 -0300

    integration test
    created json test example,
    created integration test for postmark sender

---
+++ Tests/PostmarkSenderIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using EmailServices;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class PostmarkSenderIntegrationTests
+    {
+        private PostmarkSender _postmarkSender;
+        private IConfiguration _configuration;
+        [TestInitialize]
+        public void SetUp()
+        {
+            _configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Test.json")
+            .Build();
+
+            _postmarkSender = new PostmarkSender(_configuration);
+
+        }
+        [TestMethod]
+        public async Task SendEmailAsync_WhenValidCredentials_SendEmailAsync()
+        {
+            string to = _configuration["EmailSettings:DefaultRecipient"];
+            string subjet = _configuration["EmailSettings:DefaultSubject"];
+            string body = _configuration["EmailSettings:DefaultHtmlBody"];
+            await _postmarkSender.SendEmailAsync(to, subjet, body);
+            Assert.IsTrue(true);
+        }
+    }
+}
