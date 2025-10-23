20358221ed9eb7ec8ddb1fd1d571cf59e1e60a08
sebast825 <sebamolina825@gmail.com>
2025-10-23 13:26:52 -0300

    Welcome_WhenValidConfiguration_ShouldCallSendEmailAsync
    added test project

---
+++ Tests/EmailOptionTests.cs
@@ -0,0 +1,28 @@
+﻿using Core.Interface;
+using Core.Templates;
+using EmailServices;
+using Moq;
+
+namespace Tests
+{
+    [TestClass]
+    public class EmailOptionTests
+    {
+        private  IEmailOption _emailOption;
+        private Mock<IEmailSender> _mockEmailSender;
+        [TestInitialize]
+        public void SetUp () { 
+            _mockEmailSender = new Mock<IEmailSender> ();
+            _emailOption = new EmailOption(_mockEmailSender.Object);
+        }
+
+        [TestMethod]
+        public void Welcome_WhenValidConfiguration_ShouldCallSendEmailAsync()
+        {
+            string emailReciver = "test@gmail.com";
+            _emailOption.Welcome(emailReciver);
+
+            _mockEmailSender.Verify(s => s.SendEmailAsync(emailReciver,EmailTemplatesOptions.Welcome.Subject,EmailTemplatesOptions.Welcome.HtmlBody), Times.Once);  
+        }
+    }
+}
